d952bccfccdb342a1f2e960b4c2bc551b094a388
fsziegler <fsziegler@gmail.com>
2014-06-25 12:06:14 -0700

    Added html & jpg outputs

--- Digital Music Functions.mm
+++ Digital Music Functions.mm
@@ -1,4 +1,22 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1403721937217" ID="ID_1909699675" MODIFIED="1403721954675" TEXT="Digital Music Capabilities"/>
+<node CREATED="1403721937217" ID="ID_1909699675" MODIFIED="1403722056366" TEXT="Digital Music Functions">
+<node CREATED="1403722136244" ID="ID_483187002" MODIFIED="1403722147131" POSITION="right" TEXT="Remove music file duplicates">
+<node CREATED="1403722058047" ID="ID_360782203" MODIFIED="1403722081695" TEXT="Scan multiple file systems for .mp3 files">
+<node CREATED="1403721995355" ID="ID_365557339" MODIFIED="1403722009403" TEXT="Scan a file system for .mp3 files">
+<node CREATED="1403722011698" ID="ID_565034533" MODIFIED="1403722026291" TEXT="Extract the id3 tag info"/>
+</node>
+</node>
+<node CREATED="1403722218363" ID="ID_1320776442" MODIFIED="1403722224617" TEXT="Identify duplicates"/>
+<node CREATED="1403722229373" ID="ID_1195968476" MODIFIED="1403722233311" TEXT="Remove duplicates">
+<node CREATED="1403722237138" ID="ID_570554348" MODIFIED="1403722246993" TEXT="Ask user which copy to remove">
+<node CREATED="1403722254695" ID="ID_1898340066" MODIFIED="1403722270343" TEXT="Give user option to prefer one directory over another"/>
+</node>
+<node CREATED="1403722278490" ID="ID_539992141" MODIFIED="1403722290405" TEXT="Option to consolidate music in one place"/>
+</node>
+</node>
+<node CREATED="1403722312036" ID="ID_513963103" MODIFIED="1403722316938" POSITION="right" TEXT="Standardize music files">
+<node CREATED="1403722323068" ID="ID_1828984806" MODIFIED="1403722330986" TEXT="Standardize file names"/>
+</node>
+</node>
 </map>
